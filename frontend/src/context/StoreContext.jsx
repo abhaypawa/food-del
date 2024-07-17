ef955a100a4cc78db5c7251a0fb19fef4b88f9ab
abhaypawa <144175829+abhaypawa@users.noreply.github.com>
2024-07-17 15:12:17 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 const StoreContextprovider = (props) =>{
 
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000"
+    const url = "https://food-del-backend-boq4.onrender.com"
     const [token, setToken] = useState("")
     const [food_list, setFoodList]=useState([])
     const addToCart =async (itemId) =>{
